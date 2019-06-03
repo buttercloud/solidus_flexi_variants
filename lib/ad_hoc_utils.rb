c3e53f0bba421ba5006afe30bcb0a8f72dcd33a8
Luay Bseiso <luay@buttercloud.com>
2019-06-03 12:29:17 +0300

    change the ad hoc option types and values on the product page to checkboxes

--- lib/ad_hoc_utils.rb
+++ lib/ad_hoc_utils.rb
@@ -2,7 +2,7 @@ module AdHocUtils
 
   def ad_hoc_option_value_ids # (variant_id)
     ids = []
-    params[:ad_hoc_option_values].each do |ignore, product_option_value_id|
+    params[:ad_hoc_option_values].each do |product_option_value_id|
       # pov=ProductOptionValue.find( product_option_value_id)   # we don't actually need to load these from the DB just yet.  We might already have them attached to the line item
       # when it's a multi-select
       if  product_option_value_id.is_a?(Array)
